c9b240485e3b0a6139cec33b46a8a1801a86aad8
RadiBarq <grayllow@gmail.com>
2018-01-17 21:06:03 +0200

    Initial commit

---
+++ app/src/main/java/com/gayelak/gayelakandroid/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package com.gayelak.gayelakandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+
+    public void onClickRegister(View view)
+    {
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+
+
+    }
+
+
+}
